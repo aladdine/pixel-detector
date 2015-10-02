f98f72c2595a1aed3bd209e9225f03c7813ab13a
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-10-02 00:49:59 -0700

    added color filters and counters for number of pixels with a specific colour

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var canvas = document.getElementById('canvas'),
 	context = canvas.getContext('2d'),
-	uploadedFile = document.getElementById('uploaded-file'),
-	submittedLink = document.getElementById('submitted-link'),
-	submitLink = document.getElementById('submit-link');
+	uploadedFile = document.getElementById('uploaded-file');
 
 // draws horizontal and vertical grid
 function drawGrid(orientation,delta,gridColor) {
@@ -48,13 +46,7 @@ function initImageLoader(){
 		var file = ev.target.files[0];
 		handleFile(file);
 	}
-
-	submitLink.addEventListener('click', handleSubmittedLink);
-
-	function handleSubmittedLink(ev){
-		var link = submittedLink.files[0];
-		handleFile(link);
-	}
+	
 }
 
 function handleFile(file){
@@ -64,19 +56,57 @@ function handleFile(file){
 		var reader = new FileReader();	
 		reader.onload = function(event){
 			var tempImageStrore = new Image();
-		
-			tempImageStrore.onload = function(ev){
+	
+			var returnImage = function(ev){
 				canvas.height = ev.target.height;
 				canvas.width = ev.target.width;
 				context.drawImage(ev.target, 0, 0);
 				drawGrid('horizontal',20,'red');
 				drawGrid('vertical',20,'blue');
+				image = context.getImageData(0,0,ev.target.width,ev.target.height);
+				imageFromCanvas();
 			}
+
+			tempImageStrore.onload = returnImage;
+
 		tempImageStrore.src = event.target.result;	
 		}
 	reader.readAsDataURL(file);	
 	}
 }
 
+function imageFromCanvas(){
+	var data = image.data;
+	console.log("Number of pixels = " + (data.length / 4));
+    var white = 0;
+	for (i=0; i < data.length; i+=4){
+		if ((data[i] == 0 && data[i+1]==0) && (data[i+2]==0 && data[i+3]==255)) {
+			white += 1;
+		}
+	}
 
+    console.log("Number of white pixels = " + white);
 
+	//red effect in rgb scale
+	for (i=0; i < data.length; i+=4){
+		data[i]=0;
+	}
+
+	//green effect in rgb scale
+	for (i=1; i < data.length; i+=4){
+		//data[i]=0;
+	}
+
+	// blue effect in rgb scale
+	for (i=2; i < data.length; i+=4){
+		//data[i]=0;
+	}
+
+	// opacity in rgb scale
+	for (i=3; i < data.length; i+=4){
+		//data[i]=0;
+	}
+
+	image.data = data;
+	context.putImageData(image,0,0);
+}
