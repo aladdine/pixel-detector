c5d74e2ecd9db200a3fda5e1b3de9d06de30e6df
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-10-03 16:12:01 -0700

    sync both branches

--- js/main.js
+++ js/main.js
@@ -75,7 +75,8 @@ function handleFile(file){
 				drawGrid('horizontal',20,'blue');
 				drawGrid('vertical',20,'blue');
 				image = context.getImageData(0,0,ev.target.width,ev.target.height);
-				pixelCount.innerHTML = image.data.length;			
+				// each 4 susbsequent elements of the image.data array represent one pixel
+				pixelCount.innerHTML = (image.data.length/4);			
 			}
        
 			tempImageStrore.onload = returnImage;
